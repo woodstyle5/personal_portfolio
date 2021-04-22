5dc3729395a2cb3a1df267693ca10a1622893f8c
woodstyle5 <denver00acebedo@gmail.com>
2021-04-22 11:18:13 -0400

    index-view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def project_index(request):
+	projects = Project.object.all()
+	context = {
+		'projects' : projects
+	}
+	return render(request, 'project_index.html',context)
