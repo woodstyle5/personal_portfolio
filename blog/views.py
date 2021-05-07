e2c0a3dbcd2fd4ce801d617f2906bd7b4bce2c61
woodstyle5 <denver00acebedo@gmail.com>
2021-05-07 09:46:21 -0400

    blog/view/blog_details modified

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 # Create your views here.
 def blog_index(request):
@@ -24,6 +25,18 @@ def blog_category(request,category):
 
 def blog_details(request, pk):
 	post = Post.objects.get(pk=pk)
+
+	form = CommentForm()
+	if request.method == 'POST':
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = Comment(
+				author=form.cleaned_data["author"],
+				body=form.cleaned_data["body"],
+				post=post
+			)
+			comment.save()
+
 	comments = Comment.objects.filter(post=post)
 	context = {
 		"post": post,
